95715477fc7909273630505569b56cb2245b8e07
hidetomo117 <hidetomo117@gmail.com>
2019-09-05 22:43:38 +0900

    show tableViewCell

---
+++ Swift_ShowMenuInTableView/ViewController/View/MainCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainCell.swift
+//  Swift_ShowMenuInTableView
+//
+//  Created by hidetomo on 2019/09/05.
+//  Copyright © 2019 Hidetomo Masuda. All rights reserved.
+//
+
+import UIKit
+
+class MainCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
